fcff7bdbda247b3890adc9956e3823fe5f46c747
amschel99 <6290.2020@students.ku.ac.ke>
2023-02-04 18:54:23 +0300

    basic docker commands

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express= require('express')
 const app= express()
 const PORT= 5000 || process.env.PORT
 app.get("/",(req,res)=>{
-    res.send(`<h1>hello there</h1>`)
+    res.send(`<h1>hello there!!</h1>`)
 })
 app.listen(PORT,()=>{console.log(`server listening on ${PORT}`)})
